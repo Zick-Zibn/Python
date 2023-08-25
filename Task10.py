0395155a79998bf26100492cfae96d3c2a1919a7
Zick_Zibn <karpenko_ilia@list.ru>
2023-08-25 10:35:57 +0300

    Task 10

--- Task10.py
+++ Task10.py
@@ -1,19 +1,16 @@
-x = int(input('Введите первое натуральное число X от 1 до 1000 '))
-y = int(input('Введите второе натуральное число Y от 1 до 1000 '))
-if x < 1 or x > 1000 or y < 1 or y > 1000:
-    print('Вы ввели число не из заданного диапазона!')
+qty = int(input('Введите количество монет '))
+orel = 0
+reshka = 0
+
+for i in range(qty):
+    x = int(input('Орел(1) или решка(0)? '))
+    if x == 1:
+        orel += 1
+    else:
+        reshka += 1
+if orel < reshka:
+    print(f'Переверните {orel} монет с орла на решку, их меньше всего')
+elif orel == reshka:
+    print(f'Количество орлов и решек одинаково, по {orel} штук')
 else:
-    S = x + y
-    P = x * y
-    flag = False
-    for i in range(1001):
-        if flag == False:
-            for j in range(1001):
-                if flag == False:
-                    if i * j == P and i + j == S:
-                        print(i, j)
-                        flag = True
-            else:
-                j = 1001
-        else:
-            i = 1001
+    print((f'Переверните {reshka} монет с решки на орла, их меньше всего'))            
