75ea8e9ec746ae70fc8c29779b876da2e93ad2ea
Zick_Zibn <karpenko_ilia@list.ru>
2023-08-25 10:19:39 +0300

    Task10

--- Task10.py
+++ Task10.py
@@ -1,20 +1,19 @@
-x = abs(int(input('Введите первое натуральное число X от 1 до 1000 ')))
-y = abs(int(input('Введите второе натуральное число Y от 1 до 1000 ')))
+x = int(input('Введите первое натуральное число X от 1 до 1000 '))
+y = int(input('Введите второе натуральное число Y от 1 до 1000 '))
 if x < 1 or x > 1000 or y < 1 or y > 1000:
     print('Вы ввели число не из заданного диапазона!')
 else:
     S = x + y
     P = x * y
-    stop = 0
+    flag = False
     for i in range(1001):
-        if stop != 1:
+        if flag == False:
             for j in range(1001):
-                if stop != 1:
+                if flag == False:
                     if i * j == P and i + j == S:
                         print(i, j)
-                        stop = 1
+                        flag = True
             else:
                 j = 1001
         else:
             i = 1001
-            
