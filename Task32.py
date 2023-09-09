e82e14ab9dfed36dbeca290242bffbd87a8e854b
Zick_Zibn <karpenko_ilia@list.ru>
2023-09-10 00:31:35 +0300

    Task 32

--- Task32.py
+++ Task32.py
@@ -1,12 +1,14 @@
 from random import randint
 
-list_1 = list(randint(-20, 21) for i in range(int(input('Введите размер списка: '))))
-print (list_1)
-list_2 = []
-max = 10
-min = 6
-for i in range(len(list_1)):
-    if list_1[i] >= min and list_1[i] <= max:
-        list_2.append(i)
-print(list_2)
+list_number = list(randint(-20, 21) for i in range(int(input('Введите размер списка: '))))
+print (list_number)
+list_min_max = []
+max_number = int(input('Введите верхний диапазон: '))
+min_number = int(input('Введите нижний диапазон: '))
+
+for i in range(len(list_number)):
+    if list_number[i] >= min_number and list_number[i] <= max_number:
+        list_min_max.append(i)
+
+print(list_min_max)
 
