d46e62f6212135fea4f28db9313aae84f5f8a2de
Zick_Zibn <karpenko_ilia@list.ru>
2023-09-10 23:09:57 +0300

    Task 30

---
+++ Task30.py
@@ -0,0 +1,8 @@
+
+a1 = int(input("Введите первый элемент: "))
+d  = int(input("Введите разность: "))
+n  = int(input("Введите количество элементов: "))
+
+list = [a1 + (i - 1) * d for i in range(1, n + 1)]
+
+print(*list)
