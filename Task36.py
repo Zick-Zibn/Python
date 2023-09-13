5671929054cda695bf0d8d806ecd9ed1ae05641c
Zick_Zibn <karpenko_ilia@list.ru>
2023-09-13 23:54:11 +0300

    Task 36

--- Task36.py
+++ Task36.py
@@ -1,7 +1,9 @@
-def print_operation_table(operation, num_rows=6, num_columns=6):
+def print_operation_table(operation, num_rows, num_columns):
     a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
     for i in a:
         print(*[f"{x:>3}" for x in i])
 
+num_rows = int(input("Введите количество строк: "))
+num_columns = int(input("Введите количество столбцов: "))
 
-print_operation_table(lambda x, y: x * y)
+print_operation_table(lambda x, y: x * y, num_rows, num_columns)
